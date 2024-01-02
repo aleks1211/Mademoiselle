53f7fd2f396e040d15e3ccbef5399b6d78424144
Aleksandra Kostadinovic <aleksandra.kosta.sigma@gmail.com>
2024-01-02 22:22:30 +0100

    Home page done

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -1,7 +1,16 @@
 $(document).ready(function(){
     
-    
+   //team-slider
+   if($('.team-members-slider').length > 0){
+         $(".team-members-slider").owlCarousel({
+             responsive:{
+                 0:{
+                   items:1,
+                   margin:0
+                 }
+             }
+         });
+   }
     
     
 })
-
