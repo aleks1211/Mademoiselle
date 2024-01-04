faadc6035158c8c303b9c1520cf6f965305cc653
Aleksandra Kostadinovic <aleksandra.kosta.sigma@gmail.com>
2024-01-04 19:10:10 +0100

    Only about page left

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -11,6 +11,68 @@ $(document).ready(function(){
              }
          });
    }
+   
+   //ANIMATION
+    function animation() {
+        let  windowHeight = $(window).height();
+        let scroll = $(window).scrollTop();
+        $('.animation').each(function () {
+            let position = $(this).offset().top;
+            let animationName = $(this).attr('data-animation');
+            let delay = $(this).attr('data-delay');
+            if (position < windowHeight + scroll - 100) {
+                $(this).addClass(animationName);
+                $(this).css('animation-delay', delay);
+            }
+        });
+    }
+
+
+    $(window).scroll(function () {
+        animation();
+    });
+    animation();
     
-    
-})
+
+     //form validator
+    if ($('.contact-form').length > 0) {
+        $('.contact-form').validate({
+            highlight: function (element) {
+                $(element).addClass('is-invalid').removeClass('is-valid');
+            },
+            unhighlight: function (element) {
+                $(element).addClass('is-valid').removeClass('is-invalid');
+            },
+            rules: {
+                name: {
+                    reguired: true
+                },
+                email: {
+                    reguired: true,
+                    email: true
+                },
+                message: {
+                    reguired: true
+                }
+            },
+            messages: {
+                name: {
+                    reguired: 'The Name* field is required'
+                },
+                email: {
+                    reguired: 'The Name* field is required',
+                    email: 'Please provide a valid email address'
+                },
+                message: {
+                    reguired: 'The Message* field is required'
+                }
+            },
+
+            errorElement: 'p',
+            errorPlacement: function (error, element) {
+                error.appendTo(element.closest('.form-group').find('.error-msg'));
+            }
+
+        });
+    }
+});
